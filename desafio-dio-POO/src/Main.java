de1528dbff6921f60657b6ad6d52db7bba934076
edson1609 <edson.1996.89@gmail.com>
2023-01-27 23:34:52 -0300

    Final

--- desafio-dio-POO/src/Main.java
+++ desafio-dio-POO/src/Main.java
@@ -1,17 +1,19 @@
 import java.time.LocalDate;
 
+import brcomdiodesafio.Bootcamp;
 import brcomdiodesafio.Curso;
+import brcomdiodesafio.Dev;
 import brcomdiodesafio.Mentoria;
 
 public class Main {
     public static void main(String[] args) {
-        Curso curso1 = new Curso();
+      Curso curso1 = new Curso ();
         curso1.setTitulo("Curso java");
         curso1.setDescricao("Descrição do curso de Java");
         curso1.setCargaHoraria(8);
 
-        Curso curso2 = new Curso();
-        curso2.setTitulo();
+      Curso curso2 = new Curso();
+        curso2.setTitulo ("Curso de JS");
         curso2.setDescricao("Descrição do curso de JS");
         curso2.setCargaHoraria(4);
 
@@ -22,9 +24,48 @@ public class Main {
 
        // System.out.println(curso1);
        // System.out.println(curso2);
-        System.out.println(mentoria);
+       // System.out.println(mentoria);
+
+       Bootcamp bootcamp = new Bootcamp();
+       bootcamp.setNome("Bootcamp Java Developer");
+       bootcamp.setDescricao("Descrição do bootcamp de java");
+       bootcamp.getConteudos().add(curso1);
+       bootcamp.getConteudos().add(curso2);
+       bootcamp.getConteudos().add(mentoria);
+       
+       Dev devEdson = new Dev();
+       devEdson.setNome("Edson");
+       devEdson.inscreverBootcamp(bootcamp);
+       System.out.println("Conteúdos Inscritos Edson:" + devEdson.getConteudosInscritos());
+       devEdson.progredir();
+       devEdson.progredir();
+       System.out.println("-");
+       System.out.println("Conteúdos Inscritos Edson:" + devEdson.getConteudosInscritos());
+       System.out.println("Conteúdos Concluídos Edson:" + devEdson.getConteudosConcluidos());
+       System.out.println("XP:" + devEdson.calcularTotalXp());
+
+       System.out.println("-------");
+
+       Dev devJoyce = new Dev();
+       devJoyce.setNome("Joyce");
+       devJoyce.inscreverBootcamp(bootcamp);
+       System.out.println("Conteúdos Inscritos Joyce:" + devJoyce.getConteudosInscritos());
+       devJoyce.progredir();
+       devJoyce.progredir();
+       devJoyce.progredir();
+       System.out.println("-");
+       System.out.println("Conteúdos Inscritos Joyce:"+ devJoyce.getConteudosInscritos());
+       System.out.println("Contúdos Concluidos Joyce: " + devJoyce.getConteudosConcluidos());
+       System.out.println("XP:"+ devJoyce.calcularTotalXp());
+    }
+  }
+
+
+
+
+
+
         
 
         
-         }
-}
+         
--- desafio-dio-POO/src/brcomdiodesafio/Curso.java
+++ desafio-dio-POO/src/brcomdiodesafio/Curso.java
@@ -1,42 +1,31 @@
 package brcomdiodesafio;
+public class Curso extends Conteudo{
 
-public class Curso {
-   private String titulo;
-   private String descricao;
-   private int cargaHoraria;
+    private int cargaHoraria;
 
-   public Curso (){
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
 
-   }
-
-public String getTitulo (){
-    return titulo;
-}
-public void setTitulo(String titulo) {
-    this.titulo = titulo;
-}
+    public Curso() {
+    }
 
 
-public String getDescricao(){
-    return descricao;
-}
-public void setDescricao(String descricao) {
-    this.descricao = descricao;
-}
-public int getCargaHoraria(){
-    return cargaHoraria;
-}
-public void setCargaHoraria(int cargaHoraria) {
-    this.cargaHoraria = cargaHoraria;
-}
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
 
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
 
-public String toString() {
-    return "Curso{" +
-            "titulo='" + getTitulo() + '\'' +
-            ", descricao='" + getDescricao() + '\'' +
-            ", cargaHoraria=" + cargaHoraria +
-            '}';
-}          
-    
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
 }
